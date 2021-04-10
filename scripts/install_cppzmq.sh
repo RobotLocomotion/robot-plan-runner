446d7a206a1f949c1c6de2aa638a04734dda5daa
Pang Tao <pangtao@mit.edu>
2021-04-10 01:06:11 -0400

    git action with docker

--- scripts/install_cppzmq.sh
+++ scripts/install_cppzmq.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+set -euxo pipefail
 
 # install cppzmq
 curl -o cppzmq.tar.gz -L https://github.com/zeromq/cppzmq/archive/refs/tags/v4.7.1.tar.gz
-sudo tar -xzf cppzmq.tar.gz -C /
-cd /cppzmq-4.7.1 && mkdir build && cd build
+sudo tar -xzf cppzmq.tar.gz -C / && rm cppzmq.tar.gz
+
+pushd /cppzmq-4.7.1
+mkdir build
+pushd build
 cmake -DCPPZMQ_BUILD_TESTS=OFF ..
 make install
 
-
+popd
+popd
