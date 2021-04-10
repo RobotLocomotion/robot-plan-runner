9bc7df8eb8574985448001078b1c688dab628d23
Pang Tao <pangtao@mit.edu>
2021-04-10 01:14:29 -0400

    fix scripts.

--- scripts/build_test.sh
+++ scripts/build_test.sh
@@ -3,13 +3,8 @@ set -euxo pipefail
 
 # build
 mkdir /robot-plan-runner-build && cd /robot-plan-runner-build
-cmake -DCMAKE_PREFIX_PATH=/opt/drake /robot-plan-runner
+cmake -DCMAKE_PREFIX_PATH=/opt/drake  /github/workspace
 make -j
 
-# run test
-ctest
-exit_status=$?
-if [ ! $exit_status -eq 0 ]; then
-  echo "Error code in running unit tests: " $exit_status
-  exit $exit_status
-fi
+# run tests
+ctest -V .
