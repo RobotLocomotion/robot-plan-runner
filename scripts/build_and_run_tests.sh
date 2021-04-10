e2b99774ca0ddfc3a23972b4e6416c0512cc01e3
Pang Tao <pangtao@mit.edu>
2021-04-09 22:03:42 -0400

    travis -> github actions

--- scripts/build_and_run_tests.sh
+++ scripts/build_and_run_tests.sh
@@ -1,15 +1,14 @@
-#!/usr/bin/env bash
+#! /bin/bash
 
-#git clone https://github.com/RobotLocomotion/robot-plan-runner.git
-# build
-mkdir /robot-plan-runner-build && cd /robot-plan-runner-build
-cmake -DCMAKE_PREFIX_PATH=/opt/drake /robot-plan-runner
+set -euxo pipefail
+export PYTHONPATH=/opt/drake/lib/python3.6/site-packages:$PYTHONPATH
+
+mkdir ./build
+pushd build
+
+cmake -DCMAKE_PREFIX_PATH=/opt/drake ..
 make -j
+ctest -V .
 
-# run test
-ctest
-exit_status=$?
-if [ ! $exit_status -eq 0 ]; then
-  echo "Error code in running unit tests: " $exit_status
-  exit $exit_status
-fi
+popd
+rm -rf build
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,10 +1,23 @@
-#!/usr/bin/env bash
+#! /bin/bash
+set -euxo pipefail
+export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 DEBIAN_FRONTEND=noninteractive
+
+apt-get install -o APT::Acquire::Retries=4 -o Dpkg::Use-Pty=0 -qy --no-install-recommends xvfb ca-certificates libzmq3-dev libgtest-dev libgflags-dev
+trap 'rm -rf /var/cache/apt/archives/*.deb /var/cache/apt/archives/partial/*.deb /var/cache/apt/*.bin /var/lib/apt/lists/* /var/log/apt/*' EXIT
+
+pushd /opt
+wget -nv https://drake-packages.csail.mit.edu/drake/nightly/drake-latest-bionic.tar.gz
+trap 'rm -rf /opt/drake-latest-bionic.tar.gz /var/cache/apt/archives/*.deb /var/cache/apt/archives/partial/*.deb /var/cache/apt/*.bin /var/lib/apt/lists/* /var/log/apt/*' EXIT
+tar -xf drake-latest-bionic.tar.gz
+popd
+
+apt-get install -o APT::Acquire::Retries=4 -o Dpkg::Use-Pty=0 -qy --no-install-recommends $(cat /opt/drake/share/drake/setup/packages-bionic.txt)
 
 # install cppzmq
+pushd /opt
 curl -o cppzmq.tar.gz -L https://github.com/zeromq/cppzmq/archive/refs/tags/v4.7.1.tar.gz
-sudo tar -xzf cppzmq.tar.gz -C /
-cd /cppzmq-4.7.1 && mkdir build && cd build
+tar -xzf cppzmq.tar.gz -C
+popd
+cd /opt/cppzmq-4.7.1 && mkdir build && cd build
 cmake -DCPPZMQ_BUILD_TESTS=OFF ..
 make install
-
-
