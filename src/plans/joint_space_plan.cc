715eb0a078a809179f0712e0ed9d9dcca983b785
Terry Suh <hjsuh94@gmail.com>
2021-04-21 13:57:08 -0400

    address review comments

--- src/plans/joint_space_plan.cc
+++ src/plans/joint_space_plan.cc
@@ -1,6 +1,6 @@
 #include "joint_space_plan.h"
 
-void JointSpacePlan::Step(const State &state, double control_period, double t,
+void JointSpacePlan::Step(const State &, double control_period, double t,
                           Command *cmd) const {
   cmd->q_cmd = q_traj_.value(t);
   cmd->tau_cmd = Eigen::VectorXd::Zero(q_traj_.rows());
--- src/plans/task_space_trajectory_plan.h
+++ src/plans/task_space_trajectory_plan.h
@@ -1,31 +1,35 @@
 #pragma once
 
-#include <math.h>
+#include <cmath>
 
-#include "drake/common/trajectories/piecewise_quaternion.h"
 #include "drake/common/trajectories/piecewise_polynomial.h"
+#include "drake/common/trajectories/piecewise_quaternion.h"
 #include "drake/manipulation/planner/differential_inverse_kinematics_integrator.h"
+#include "drake/math/rigid_transform.h"
 #include "drake/multibody/plant/multibody_plant.h"
 #include "drake/multibody/tree/multibody_tree.h"
 #include "drake/systems/framework/context.h"
-#include "drake/math/rigid_transform.h"
 
 #include "plan_base.h"
 
-using drake::manipulation::planner::DifferentialInverseKinematicsParameters;
-
 class TaskSpaceTrajectoryPlan : public PlanBase {
 public:
-  TaskSpaceTrajectoryPlan(drake::trajectories::PiecewiseQuaternionSlerp<double> quat_traj,
-  drake::trajectories::PiecewisePolynomial<double> xyz_traj,
-  const drake::multibody::MultibodyPlant<double> *plant)
-      : PlanBase(plant), quat_traj_(std::move(quat_traj)), xyz_traj_(std::move(xyz_traj)) {
+  TaskSpaceTrajectoryPlan(
+      drake::trajectories::PiecewiseQuaternionSlerp<double> quat_traj,
+      drake::trajectories::PiecewisePolynomial<double> xyz_traj,
+      const drake::multibody::MultibodyPlant<double> *plant)
+      : PlanBase(plant), quat_traj_(std::move(quat_traj)),
+        xyz_traj_(std::move(xyz_traj)) {
 
-        params_ = new DifferentialInverseKinematicsParameters(
-          plant_->num_positions(), plant_->num_velocities());
+    params_ =
+        std::move(std::unique_ptr<drake::manipulation::planner::
+                                      DifferentialInverseKinematicsParameters>(
+            new drake::manipulation::planner::
+                DifferentialInverseKinematicsParameters(
+                    plant_->num_positions(), plant_->num_velocities())));
 
-        plant_context_ = plant_->CreateDefaultContext();
-      }
+    plant_context_ = plant_->CreateDefaultContext();
+  }
 
   ~TaskSpaceTrajectoryPlan() override = default;
 
@@ -33,13 +37,15 @@ public:
             Command *cmd) const override;
 
   [[nodiscard]] double duration() const override {
-    return xyz_traj_.end_time() - xyz_traj_.start_time();};
+    return xyz_traj_.end_time() - xyz_traj_.start_time();
+  };
 
 private:
   const drake::trajectories::PiecewiseQuaternionSlerp<double> quat_traj_;
   const drake::trajectories::PiecewisePolynomial<double> xyz_traj_;
 
   std::unique_ptr<drake::systems::Context<double>> plant_context_;
-
-  DifferentialInverseKinematicsParameters* params_;
+  std::unique_ptr<
+      drake::manipulation::planner::DifferentialInverseKinematicsParameters>
+      params_;
 };
