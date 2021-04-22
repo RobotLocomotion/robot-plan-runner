6eb0887855c86020a7cdaa98366f1e4fcf8e128d
Terry Suh <hjsuh94@gmail.com>
2021-04-22 12:08:36 -0400

    fix build error

--- src/plans/iiwa_plan_factory.cc
+++ src/plans/iiwa_plan_factory.cc
@@ -20,7 +20,7 @@ IiwaPlanFactory::IiwaPlanFactory(const YAML::Node &config) : config_(config) {
   plant_ = std::make_unique<drake::multibody::MultibodyPlant<double>>(1e-3);
   auto parser = drake::multibody::Parser(plant_.get());
 
-  const std::string iiwa_path = 
+  std::string iiwa_path = 
     config_["robot_sdf_path"].as<std::string>();
   iiwa_path = drake::FindResourceOrThrow(iiwa_path);
   parser.AddModelFromFile(iiwa_path);
