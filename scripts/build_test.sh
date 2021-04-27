8020bbbe704a274b5caa44a928472d8559579115
Unknown <pangtao@mit.edu>
2021-04-27 05:00:16 -0400

    build own lcm types locally and include in sandbox..

--- scripts/build_test.sh
+++ scripts/build_test.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# build
+# build lcm types
+bash /github/workspace/lcmtypes/install_lcm_types_here.sh
+
+# build plan runner
 mkdir /robot-plan-runner-build && cd /robot-plan-runner-build
 cmake -DCMAKE_PREFIX_PATH=/opt/drake  /github/workspace
 make -j
