c23c344d4e2176a282f40ee24ffa29c20a831830
mpetersen94 <markpetersen@g.harvard.edu>
2021-04-27 17:53:12 +0000

    Fix build error

--- scripts/build_test.sh
+++ scripts/build_test.sh
@@ -5,7 +5,6 @@ set -euxo pipefail
 mkdir /robot-plan-runner-build && cd /robot-plan-runner-build
 cmake -DCMAKE_PREFIX_PATH=/opt/drake  /github/workspace
 make -j
-export PYTHON_PATH=$PYTHON_PATH:/robot-plan-runner-build/robot-plan-runner-lcmtypes
 
 # run tests
 ctest -V .
