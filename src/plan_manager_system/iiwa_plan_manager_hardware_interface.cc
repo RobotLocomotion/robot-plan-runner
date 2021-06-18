c623802a83f88ee0680a29c43b7d6d8acb59a868
Unknown <pangtao@mit.edu>
2021-06-18 05:54:33 -0400

    Publish command with drake::lcm::Publish.

--- src/plan_manager_system/iiwa_plan_manager_hardware_interface.cc
+++ src/plan_manager_system/iiwa_plan_manager_hardware_interface.cc
@@ -1,33 +1,18 @@
 #include <fstream>
 #include <numeric>
 
-#include "drake/systems/analysis/simulator.h"
 #include "drake/systems/lcm/lcm_interface_system.h"
-#include "drake/systems/lcm/lcm_publisher_system.h"
-#include "drake/systems/lcm/lcm_subscriber_system.h"
 
 #include "iiwa_plan_manager_hardware_interface.h"
 
 IiwaPlanManagerHardwareInterface::IiwaPlanManagerHardwareInterface(
     const YAML::Node &config) {
-
-  const auto control_period_seconds = config["control_period"].as<double>();
-
   owned_lcm_ = std::make_unique<drake::lcm::DrakeLcm>();
 
   drake::systems::DiagramBuilder<double> builder;
 
   // PlanManagerSystem.
   plan_manager_ = builder.template AddSystem<IiwaPlanManagerSystem>();
-
-  // Publish iiwa command.
-  auto iiwa_command_pub = builder.AddSystem(
-      drake::systems::lcm::LcmPublisherSystem::Make<drake::lcmt_iiwa_command>(
-          config["lcm_command_channel"].as<std::string>(), owned_lcm_.get(),
-          control_period_seconds));
-  builder.Connect(plan_manager_->get_iiwa_command_output_port(),
-                  iiwa_command_pub->get_input_port());
-
   diagram_ = builder.Build();
 
   // A standalone subscriber to IIWA_STATUS, used for lcm-driven loop.
@@ -38,14 +23,14 @@ IiwaPlanManagerHardwareInterface::IiwaPlanManagerHardwareInterface(
 
 [[noreturn]] void IiwaPlanManagerHardwareInterface::Run() {
   auto context_diagram = diagram_->CreateDefaultContext();
-  auto& context_manager = plan_manager_->GetMyMutableContextFromRoot(
-      context_diagram.get());
+  auto &context_manager =
+      plan_manager_->GetMyMutableContextFromRoot(context_diagram.get());
 
-  // wait for the first IIWA_STATUS message.
+  // Wait for the first IIWA_STATUS message.
   drake::log()->info("Waiting for first lcmt_iiwa_status");
   drake::lcm::LcmHandleSubscriptionsUntil(
       owned_lcm_.get(), [&]() { return status_sub_->count() > 0; });
-  auto& status_value = plan_manager_->get_iiwa_status_input_port().FixValue(
+  auto &status_value = plan_manager_->get_iiwa_status_input_port().FixValue(
       &context_manager, status_sub_->message());
   const double t_start = status_sub_->message().utime * 1e-6;
 
@@ -55,10 +40,16 @@ IiwaPlanManagerHardwareInterface::IiwaPlanManagerHardwareInterface(
     // Wait for an IIWA_STATUS message.
     drake::lcm::LcmHandleSubscriptionsUntil(
         owned_lcm_.get(), [&]() { return status_sub_->count() > 0; });
+
+    // Update diagram context.
     status_value.GetMutableData()->set_value(status_sub_->message());
     const double t = status_sub_->message().utime * 1e-6;
     context_diagram->SetTime(t - t_start);
-    diagram_->Publish(*context_diagram);
+
+    // Compute command message and publish.
+    auto iiwa_cmd_msg = plan_manager_->get_iiwa_command_output_port()
+                            .Eval<drake::lcmt_iiwa_command>(context_manager);
+    drake::lcm::Publish(owned_lcm_.get(), "IIWA_COMMAND", iiwa_cmd_msg);
   }
 }
 
