2551eff898134f31124083f867888ff742760c83
Hyung Ju Suh (Terry) <hjsuh94@gmail.com>
2021-08-02 17:17:46 -0400

    Add SchunkManager (#37)

--- src/plan_runner_client/zmq_client.py
+++ src/plan_runner_client/zmq_client.py
@@ -1,6 +1,7 @@
 import time
 import threading
 import sys
+import copy
 
 import zmq
 import lcm
@@ -9,7 +10,9 @@ import numpy as np
 
 from pydrake.all import MultibodyPlant, Parser, RigidTransform
 from pydrake.common import FindResourceOrThrow
-from drake import lcmt_iiwa_status, lcmt_robot_plan
+from drake import (
+    lcmt_iiwa_status, lcmt_robot_plan,
+    lcmt_schunk_wsg_status, lcmt_schunk_wsg_command)
 
 from robot_plan_runner import lcmt_plan_status, lcmt_plan_status_constants
 
@@ -38,21 +41,27 @@ class IiwaPositionGetter:
         sub = self.lc.subscribe("IIWA_STATUS", self.sub_callback)
         sub.set_queue_capacity(1)
         self.iiwa_position_measured = None
+        self.msg_lock = threading.Lock()
         self.t1 = threading.Thread(target=self.update_iiwa_position_measured)
         self.t1.start()
 
     def sub_callback(self, channel, data):
         iiwa_status_msg = lcmt_iiwa_status.decode(data)
+        self.msg_lock.acquire()
         self.iiwa_position_measured = iiwa_status_msg.joint_position_measured
+        self.msg_lock.release()
 
     def update_iiwa_position_measured(self):
         while True:
             self.lc.handle()
 
     def get_iiwa_position_measured(self):
+        iiwa_position_measured = np.array([])
+        self.msg_lock.acquire()
         if self.iiwa_position_measured:
-            return np.array(self.iiwa_position_measured)
-        return np.array([])
+            iiwa_position_measured = np.array(self.iiwa_position_measured)
+        self.msg_lock.release()
+        return iiwa_position_measured
 
 
 class PlanManagerZmqClient:
@@ -88,6 +97,10 @@ class PlanManagerZmqClient:
         # iiwa7 plant.
         self.plant = build_iiwa7_plant()
 
+        # names of the enum in lcmt_plan_status_constants
+        self.plan_stats_dict = {
+            0: "RUNNING", 1: "DISCARDED", 2: "ERROR", 3: "FINISHED"}
+
     def subscribe_to_status(self):
         while True:
             msg = self.status_subscriber.recv()
@@ -96,6 +109,12 @@ class PlanManagerZmqClient:
             self.status_msg_lock.acquire()
             self.last_status_msg = lcm_msg
             self.status_msg_lock.release()
+
+    def get_plan_status(self):
+        self.status_msg_lock.acquire()
+        status_msg = copy.deepcopy(self.last_status_msg)
+        self.status_msg_lock.release()
+        return status_msg
 
     def get_current_ee_pose(self, frame_E):
         context = self.plant.CreateDefaultContext()
@@ -109,9 +128,9 @@ class PlanManagerZmqClient:
 
     def send_plan(self, plan_msg):
         self.plan_msg_lock.acquire()
-        self.last_plan_msg = plan_msg
+        self.last_plan_msg = copy.deepcopy(plan_msg)
         self.plan_msg_lock.release()
-        self.plan_client.send(self.last_plan_msg.encode())
+        self.plan_client.send(plan_msg.encode())
         msg = self.plan_client.recv()
         assert msg == b'plan_received'
         print("plan received by server.")
@@ -119,25 +138,79 @@ class PlanManagerZmqClient:
     def wait_for_plan_to_finish(self):
         # TODO: add timeout.
         while True:
-            self.status_msg_lock.acquire()
+            status_msg = self.get_plan_status()
             self.plan_msg_lock.acquire()
             is_same_plan = (
-                self.last_plan_msg.utime == self.last_status_msg.utime)
+                self.last_plan_msg.utime == status_msg.utime)
             is_plan_finished = (
-                self.last_status_msg.status ==
+                status_msg.status ==
                 lcmt_plan_status_constants.FINISHED)
             is_plan_error = (
-                self.last_status_msg.status ==
+                status_msg.status ==
                 lcmt_plan_status_constants.ERROR)
             self.plan_msg_lock.release()
-            self.status_msg_lock.release()
 
             if is_same_plan and (is_plan_finished or is_plan_error):
                 break
             time.sleep(0.01)
-        print("Final status:", self.last_status_msg.status)
+        print("Final status:", self.plan_stats_dict[status_msg.status])
 
     def abort(self):
         self.abort_client.send(b"abort")
         s = self.abort_client.recv_string()
         print(s)
+
+
+class SchunkManager:
+    def __init__(self, force_limit=40.0):
+        self.lc = lcm.LCM()
+        sub = self.lc.subscribe("SCHUNK_WSG_STATUS", self.sub_callback)
+        sub.set_queue_capacity(1)
+        self.schunk_position_measured = None
+        self.schunk_position_commanded = None
+        self.msg_lock = threading.Lock()
+        self.t1 = threading.Thread(target=self.update_schunk_position_measured)
+        self.t1.start()
+        self.force_limit = force_limit
+
+    def sub_callback(self, channel, data):
+        schunk_status_msg = lcmt_schunk_wsg_status.decode(data)
+        self.msg_lock.acquire()
+        self.schunk_position_measured = schunk_status_msg.actual_position_mm
+        self.msg_lock.release()
+
+    def update_schunk_position_measured(self):
+        while True:
+            self.lc.handle()
+
+    def get_schunk_position_measured(self):
+        self.msg_lock.acquire()
+        p = np.array(self.schunk_position_measured)
+        self.msg_lock.release()
+        return p
+
+    def send_schunk_position_command(self, command_mm):
+        self.schunk_position_commanded = command_mm
+        msg = lcmt_schunk_wsg_command()
+        msg.utime = 0 # ignore time?
+        msg.target_position_mm = command_mm
+        msg.force = self.force_limit
+        self.lc.publish("SCHUNK_WSG_COMMAND", msg.encode())
+
+    def wait_for_command_to_finish(self):
+        # NOTE(terry-suh): Note that when doing a grasp, the commanded position
+        # typically penetrates the object, and therefore the actual position 
+        # will never reach the desired position, causing this method to hang
+        # indefinitely. In this case, it is more advisable to simply wait for 
+        # a set time using time.sleep instead of calling this method.
+        time_now = time.time()
+        while True:
+            reached_goal = (
+                np.abs(self.get_schunk_position_measured() -
+                       self.schunk_position_commanded) < 0.5)
+            if reached_goal:
+                print("Schunk command is successfully executed.")
+                break
+            if (time.time() - time_now) > 60.0:
+                print("Timeout. 60 seconds elapsed but Schunk failed to reach.")
+                break
